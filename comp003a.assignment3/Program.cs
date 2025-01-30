53f9afecfb9008f71352fc6255ad65fb6b8ac5e8
Your Name <you@example.com>
2025-01-29 16:40:22 -0800

    Add project files.

---
+++ comp003a.assignment3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Author: Roman
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Budget management application demonstrating control flow.
+namespace comp003a.assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
