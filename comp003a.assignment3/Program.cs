7a60d3a8909cd7a800b94cad6a29d4905866a84d
Your Name <you@example.com>
2025-01-29 17:30:28 -0800

    added the first part of the code

--- comp003a.assignment3/Program.cs
+++ comp003a.assignment3/Program.cs
@@ -8,7 +8,28 @@ namespace comp003a.assignment3
     {
         static void Main(string[] args)
         {
-           
+            double monthlyIncome = 0;
+            string[] expenses = new string[5];
+            double[] expenseAmount = new double[5];
+            int count = 0;
+
+            Console.Write("Enter your monthly budget:");
+            monthlyIncome = double.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                Console.WriteLine("\n1. Add an Expense");
+                Console.WriteLine("2. View expenses and budget");
+                Console.WriteLine("3. Remove and expense");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("Choose and option");
+
+            }
+
+
+
+
+
         }
     }
 }
