898876f753fb0735c828ff79634fcdb2acc31d9c
Your Name <you@example.com>
2025-01-30 23:18:29 -0800

    finished with case 2 and gonna test if it works

--- comp003a.assignment3/Program.cs
+++ comp003a.assignment3/Program.cs
@@ -23,6 +23,46 @@ namespace comp003a.assignment3
                 Console.WriteLine("3. Remove and expense");
                 Console.WriteLine("4. Exit");
                 Console.WriteLine("Choose and option");
+                int choice = Convert.ToInt32(Console.ReadLine()); // converts the users input into a int
+
+                switch (choice)
+                {
+                    case 1:
+                        if (count >= 5)
+                        {
+                            Console.WriteLine("you can only add up to 5 expenses.");
+                            break;
+                        }
+
+                        Console.Write("Enter expense name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Enter the amount of the expense: ");
+                        double amount = Convert.ToDouble(Console.ReadLine());// converts the users input into a double
+
+                        expenses[count] = name;
+                        expenseAmount[count] = amount;
+                        count++;
+
+                        Console.WriteLine("Expense added");
+                        break;
+
+                    case 2:
+                        {
+                            double totalExpenses = 0; 
+                            Console.WriteLine("\nExpenses: ");
+                            for (int i = 0; i < count; i++)
+                            {
+                                Console.WriteLine($"{expenses[i]}: ${expenseAmount[i]}");
+                                totalExpenses += expenseAmount[i];
+                            }
+                            double whateverIsLeft = monthlyIncome - totalExpenses;
+                            Console.WriteLine($"\n Total Expenses: ${totalExpenses}");
+                            Console.WriteLine($"\n Remaining Budget: ${whateverIsLeft}");
+                            break;
+
+                        }
+
+                }
 
             }
 
